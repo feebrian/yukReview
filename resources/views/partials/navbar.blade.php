55d65a4c2999a3808650912a47cea8c2e285e3a3
feebrian <adamaditama13@gmail.com>
2023-04-01 02:35:27 +0700

    initial commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<header class="bg-violet-600">
+    <nav class="container py-2 flex justify-between items-center">
+        <div class="logo">
+            <a class="font-bold italic text-white text-2xl">YukReview</a>
+        </div>
+        <div class="navbar-nav">
+            <ul class="navbar text-white font-semibold flex gap-2 items-center">
+                <li><a href="#">home</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">categories</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
